88aa733bb158d21d9fa7a65701c5f1d1c128e9e4
sun.l <820602086@qq.com>
2018-07-14 11:18:00 +0800

    课程lesson模块

--- server/admin/src/main/java/com/lamport/admin/service/FreeListenService.java
+++ server/admin/src/main/java/com/lamport/admin/service/FreeListenService.java
@@ -19,7 +19,7 @@ public interface FreeListenService {
 	 * @return 1 保存成功 0 保存失败
 	 * @throws Exception
 	 */
-	public int saveFreeListen(FreeListen freeListen, MultipartFile img) throws Exception;
+	public int saveFreeListen(FreeListen freeListen, MultipartFile img, String path) throws Exception;
 	/**
 	 * 通过id逻辑删除FreeListen信息
 	 * @param id
@@ -33,7 +33,7 @@ public interface FreeListenService {
 	 * @return 1 更新成功 0 更新失败
 	 * @throws Exception
 	 */
-	public int updateFreeListenByID(FreeListen freeListen, MultipartFile img) throws Exception;
+	public int updateFreeListenByID(FreeListen freeListen, MultipartFile img, String path) throws Exception;
 	/**
 	 * 通过多条件查询FreeListen信息
 	 * @return
--- server/admin/src/main/java/com/lamport/admin/service/impl/FreeListenServiceBean.java
+++ server/admin/src/main/java/com/lamport/admin/service/impl/FreeListenServiceBean.java
@@ -1,15 +1,22 @@
 package com.lamport.admin.service.impl;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.lamport.admin.mapper.AddressMapper;
 import com.lamport.admin.mapper.FreeListenBookMapper;
 import com.lamport.admin.mapper.FreeListenMapper;
+import com.lamport.admin.po.Address;
 import com.lamport.admin.po.FreeListen;
+import com.lamport.admin.po.Lesson;
+import com.lamport.admin.po.LessonBranch;
 import com.lamport.admin.service.FreeListenService;
+import com.lamport.admin.tool.FileTool;
 import com.lamport.admin.vo.FreeListenQueryCondition;
 
 /**
@@ -25,29 +32,80 @@ public class FreeListenServiceBean implements FreeListenService {
 	private FreeListenBookMapper freeListenBookMapper;	
 	@Autowired
 	private FreeListenMapper freeListenMapper;
+	@Autowired
+	private AddressMapper addressMapper;
 	
 	@Override
-	public int saveFreeListen(FreeListen freeListen, MultipartFile img) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+	public int saveFreeListen(FreeListen freeListen, MultipartFile img, String path) throws Exception {
+		int saveResult = 1;
+		
+		String imgurl = null;
+		File imgFile = null;
+		if(img != null) {
+			File f = new File(path);
+			String ppath = f.getParent();
+			String filename = System.currentTimeMillis() + img.getOriginalFilename();
+			imgFile = new File(ppath + "img/teacher", filename);
+			imgurl = imgFile.getPath();
+		}
+		
+		freeListen.setImgurl(imgurl);
+		freeListen.setDeletekey(0);
+		
+		saveResult = freeListenMapper.saveFreeListen(freeListen);
+		
+		if(imgurl != null) {
+			img.transferTo(imgFile);
+		}
+		return saveResult;
 	}
 
 	@Override
 	public int deleteFreeListenLogicallyByID(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		int deleteResult = 0;
+		
+		deleteResult = freeListenMapper.deleteFreeListenLogicallyByID(id);
+		
+		return deleteResult;
 	}
 
 	@Override
-	public int updateFreeListenByID(FreeListen freeListen, MultipartFile img) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+	public int updateFreeListenByID(FreeListen freeListen, MultipartFile img, String path) throws Exception {
+	int updateResult = 1;
+		
+		String imgurl = null;
+		File imgFile = null;
+		if(img != null){
+			File f = new File(path);
+			String ppath = f.getParent();
+			String filename = System.currentTimeMillis() + img.getOriginalFilename();
+			imgFile = new File(ppath + "/img/lesson", filename);
+			imgurl = imgFile.getPath();
+		}
+		String oldImgurl = freeListenMapper.selectFreeListenImgurlByID(freeListen.getId());
+		freeListen.setImgurl(imgurl);
+		updateResult = freeListenMapper.updateFreeListenByID(freeListen);
+		if(imgurl != null){
+			img.transferTo(imgFile);//保存文件
+			FileTool.deleteFile(oldImgurl);
+		}
+		return updateResult;
 	}
 
 	@Override
 	public List<FreeListen> selectFreeListenByFreeListenQueryCondition(FreeListenQueryCondition freeListenQueryCondition) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<FreeListen> freeListens = null;
+		List<FreeListen> freeListens_query = null;
+
+		freeListens = freeListenMapper.selectFreeListenByFreeListenQueryCondition(freeListenQueryCondition);
+		for(FreeListen freeListen : freeListens){
+			Address address = addressMapper.selectAddressByID(freeListen.getBranchid());
+			freeListen.setBeanch(address);
+			if(address.getBranch().equals(freeListenQueryCondition.getBranch())) {
+				freeListens_query.add(freeListen);
+			}
+		}
+		return freeListens_query;
 	}
 
 	@Override
