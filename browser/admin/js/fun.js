991f51ed0bc31a47ca94dd1e3a202444bc2238b7
sun.l <820602086@qq.com>
2018-08-07 22:41:42 +0800

    0807 2nd merge

--- browser/admin/js/fun.js
+++ browser/admin/js/fun.js
@@ -14,7 +14,6 @@ $.ajaxSetup({
 	}
 });
 
-
 //将顶端导航栏弄消失  往上滚动出现  往下滚动消失
 var scrolltop = new Array();
 var i_sc = 0;
@@ -29,7 +28,6 @@ $("#content_div").scroll(function() {
 		$("header_").fadeIn()
 	};
 })
-
 
 function redirectedFunc(res) {
 	if((res != null) && ("REDIRECT" == res.getResponseHeader("REDIRECT"))) { //若HEADER中含有REDIRECT说明后端想重定向，
@@ -102,10 +100,9 @@ function layer_out(content) {
 			title: '',
 			content: content,
 			icon: 2,
-			closeBtn: false,
+			closeBtn: true,
 			anim: 6,
-			btn: false,
-			time: 1200
+			btn: false
 		});
 	});
 }
@@ -125,18 +122,18 @@ function required_verify(posi, value) {
  * @param {Object} expression	正则表达式
  * @param {Object} info 提示信息
  */
-function verify_(posi, value, expression, info){
+function verify_(posi, value, expression, info) {
 	console.log(posi);
 	console.log(value);
 	console.log(expression);
 	console.log(info);
 	console.log(expression.test(value));
-	if(! expression.test(value)){
+	if(!expression.test(value)) {
 		layer_out(posi + ": " + info);
-		console.log("----------------------------------验证失败" + value);
+		console.log("----------------------------------验证失败" + value + " " + expression);
 		return 0;
 	} else {
-		console.log("----------------------------------验证成功" + value);
+		console.log("----------------------------------验证成功" + value + " " + expression);
 	}
 	return 1;
 }
@@ -164,127 +161,70 @@ function getCategories(id_) {
 }
 
 function getCategories(id_, type) {
-	jqxhr = $.ajax({
-		type:"post",
-		url:PREPATH + "/admin/getCategories",
-		async:true,
-		dataType: "json",
-		success: function(data) {
-			var values = data.values;
-			$.each(values, function(index) {
-				if(type == 1) {
-					$("#" + id_).append('<option value="' + values[index] + '">' + values[index] + '</option>');
-				} else if (type == 2) {
-					$("#" + id_).append('<input type="checkbox" name="' + values[index] + '" title="' + values[index] + 'C++">"');			
-				}
-			});
-		}
-	});	
+	layui.use("form", function() {
+		var form = layui.form;
+
+		jqxhr = $.ajax({
+			type: "post",
+			url: PREPATH + "/admin/getCategories",
+			async: true,
+			dataType: "json",
+			success: function(data) {
+				var values = data.values;
+				$.each(values, function(index) {
+					if(type == 1) {
+						$("#" + id_).append('<option value="' + values[index] + '">' + values[index] + '</option>');
+					} else if(type == 2) {
+						$("#" + id_).append('<input type="checkbox" name="' + values[index] + '" title="' + values[index] + '">"');
+					}
+				});
+				form.render();
+			},
+			error: function() {
+				$("#" + id_).append('<option value="课程类别信息获取失败，请重试或联系系统管理员" disabled>课程类别信息获取失败，请重试或联系系统管理员</option>');
+				form.render();
+			}
+		});
+	});
 }
-//图表函数
-function renderLayer04Left(data1){
-	var myChart = echarts.init(document.getElementById("layer04_left_chart"));
-	console.log("1222");
-	myChart.setOption(
-		{
-			title: {
-				text: ''
-			},
-			tooltip : {
-				trigger: 'axis'
-			},
-			legend: {
-				data:[]
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '5%',
-				top:'4%',
-				containLabel: true
-			},
-			xAxis :
-			{
-				type : 'category',
-				boundaryGap : false,
-				data : getLatestDays(30),
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-					},
-					rotate:45,
-					interval:0
-				},
-				axisTick:{show:false},
-				axisLine:{
-					show:true,
-					lineStyle:{
-						color: '#0B3148',
-						width: 1,
-						type: 'solid'
-					}
-				}
-			},
-			yAxis : 
-			{
-				type : 'value',
-				axisTick:{show:false},
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-						}
-				},
-				axisLine:{
-					show:true,
-					lineStyle:{
-						color: '#0B3148',
-						width: 1,
-						type: 'solid'
-					}
-				},
-				splitLine:{
-					show:false
-				}
-			},
-			tooltip:{
-				formatter:'{c}',
-				backgroundColor:'#FE8501'
-			},
-			series : [
-				{
-					name:'',
-					type:'line',
-					smooth:true,
-					areaStyle:{
-						normal:{
-							color:new echarts.graphic.LinearGradient(0, 0, 0, 1, [{offset: 0, color: '#026B6F'}, {offset: 1, color: '#012138' }], false),
-							opacity:0.2
-						}
-					},
-					itemStyle : {  
-                            normal : {  
-                                  color:'#009991'
-                            },
-							lineStyle:{
-								normal:{
-								color:'#009895',
-								opacity:1
-							}
-						}
-                    },
-					symbol:'none',
-					data:data1
-				}
-			]
-		}
-	
-	);
+//获取分部信息，填充到下拉框的地方
+function getBranchesName(id_) {
+	getBranchesName(id_, 1);
 }
 
-function renderLayer04Right(data2, data3){
-	var myChart = echarts.init(document.getElementById("layer04_right_chart"));
+function getBranchesName(id_, type) {
+	layui.use("form", function() {
+		var form = layui.form;
+		console.log(type);
+		jqxhr = $.ajax({
+			type: "post",
+			url: PREPATH + "/admin/getBranchesName",
+			async: true,
+			dataType: "json",
+			success: function(data) {
+				var values = data.values;
+				$.each(values, function(index) {
+					console.log(type);
+					if(type == 1) {
+						$("#" + id_).append('<option value="' + values[index] + '">' + values[index] + '</option>');
+					} else if(type == 2) {
+						$("#" + id_).append('<input type="checkbox" name="' + values[index] + '" title="' + values[index] + '">"');
+					}
+				});
+				form.render();
+			},
+			error: function() {
+				$("#" + id_).append('<option value="分部信息获取失败，请重试或联系系统管理员" disabled>分部信息获取失败，请重试或联系系统管理员</option>');
+				form.render();
+			}
+		});
+	});
+}
+
+//图表函数
+function renderLayer04Left(data1) {
+	var myChart = echarts.init(document.getElementById("layer04_left_chart"));
+	console.log("1222");
 	myChart.setOption({
 			title: {
 				text: ''
@@ -293,184 +233,292 @@ function renderLayer04Right(data2, data3){
 				trigger: 'axis'
 			},
 			legend: {
-				top:20,
-				right:5,
-				textStyle:{
-					color:'white'
-				},
-				orient:'vertical',
-				data:[
-						{name:'订单数量',icon:'circle'},
-						{name:'预约数量',icon:'circle'}
-					]
+				data: []
 			},
 			grid: {
 				left: '3%',
-				right: '16%',
-				bottom: '3%',
-				top:'3%',
+				right: '4%',
+				bottom: '5%',
+				top: '4%',
 				containLabel: true
 			},
 			xAxis: {
 				type: 'category',
 				boundaryGap: false,
-				axisTick:{show:false},
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-						}
+				data: getLatestDays(30),
+				axisLabel: {
+					textStyle: {
+						color: "white", //刻度颜色
+						fontSize: 8 //刻度大小
+					},
+					rotate: 45,
+					interval: 0
 				},
-				axisLine:{
-					show:true,
-					lineStyle:{
+				axisTick: {
+					show: false
+				},
+				axisLine: {
+					show: true,
+					lineStyle: {
 						color: '#0B3148',
 						width: 1,
 						type: 'solid'
 					}
-				},
-				data: get6Month()
+				}
 			},
 			yAxis: {
 				type: 'value',
-				axisTick:{show:false},
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-						}
+				axisTick: {
+					show: false
 				},
-				axisLine:{
-					show:true,
-					lineStyle:{
+				axisLabel: {
+					textStyle: {
+						color: "white", //刻度颜色
+						fontSize: 8 //刻度大小
+					}
+				},
+				axisLine: {
+					show: true,
+					lineStyle: {
 						color: '#0B3148',
 						width: 1,
 						type: 'solid'
 					}
 				},
-				splitLine:{
-					show:false
+				splitLine: {
+					show: false
 				}
 			},
-			series: [
-						{
-							name:'订单数量',
-							type:'line',
-							itemStyle : {  
-									normal : {  
-									color:'#F3891B'
-								},
-								lineStyle:{
-									normal:{
-									color:'#F3891B',
-									opacity:1
-										}
-								}
-							},  
-							data:data2
-						},
-						{
-							name:'预约数量',
-							type:'line',
-							itemStyle : {  
-									normal : {  
-									color:'#006AD4'
-								},
-								lineStyle:{
-									normal:{
-									color:'#F3891B',
-									opacity:1
-										}
-								}
-							},
-							data:data3
+			tooltip: {
+				formatter: '{c}',
+				backgroundColor: '#FE8501'
+			},
+			series: [{
+				name: '',
+				type: 'line',
+				smooth: true,
+				areaStyle: {
+					normal: {
+						color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+							offset: 0,
+							color: '#026B6F'
+						}, {
+							offset: 1,
+							color: '#012138'
+						}], false),
+						opacity: 0.2
+					}
+				},
+				itemStyle: {
+					normal: {
+						color: '#009991'
+					},
+					lineStyle: {
+						normal: {
+							color: '#009895',
+							opacity: 1
 						}
-					]
-		}	
+					}
+				},
+				symbol: 'none',
+				data: data1
+			}]
+		}
+
 	);
+}
+
+function renderLayer04Right(data2, data3) {
+	var myChart = echarts.init(document.getElementById("layer04_right_chart"));
+	myChart.setOption({
+		title: {
+			text: ''
+		},
+		tooltip: {
+			trigger: 'axis'
+		},
+		legend: {
+			top: 20,
+			right: 5,
+			textStyle: {
+				color: 'white'
+			},
+			orient: 'vertical',
+			data: [{
+					name: '订单数量',
+					icon: 'circle'
+				},
+				{
+					name: '预约数量',
+					icon: 'circle'
+				}
+			]
+		},
+		grid: {
+			left: '3%',
+			right: '16%',
+			bottom: '3%',
+			top: '3%',
+			containLabel: true
+		},
+		xAxis: {
+			type: 'category',
+			boundaryGap: false,
+			axisTick: {
+				show: false
+			},
+			axisLabel: {
+				textStyle: {
+					color: "white", //刻度颜色
+					fontSize: 8 //刻度大小
+				}
+			},
+			axisLine: {
+				show: true,
+				lineStyle: {
+					color: '#0B3148',
+					width: 1,
+					type: 'solid'
+				}
+			},
+			data: get6Month()
+		},
+		yAxis: {
+			type: 'value',
+			axisTick: {
+				show: false
+			},
+			axisLabel: {
+				textStyle: {
+					color: "white", //刻度颜色
+					fontSize: 8 //刻度大小
+				}
+			},
+			axisLine: {
+				show: true,
+				lineStyle: {
+					color: '#0B3148',
+					width: 1,
+					type: 'solid'
+				}
+			},
+			splitLine: {
+				show: false
+			}
+		},
+		series: [{
+				name: '订单数量',
+				type: 'line',
+				itemStyle: {
+					normal: {
+						color: '#F3891B'
+					},
+					lineStyle: {
+						normal: {
+							color: '#F3891B',
+							opacity: 1
+						}
+					}
+				},
+				data: data2
+			},
+			{
+				name: '预约数量',
+				type: 'line',
+				itemStyle: {
+					normal: {
+						color: '#006AD4'
+					},
+					lineStyle: {
+						normal: {
+							color: '#F3891B',
+							opacity: 1
+						}
+					}
+				},
+				data: data3
+			}
+		]
+	});
 }
 
 function get6Month() {
 	var last6Month = new Array();
-	var data=new Date();  
-                //获取年  
-                var year=data.getFullYear();  
-                //获取月  
-                var mon=data.getMonth()+1; 
-                 if(mon<=0){  
-                        year=year-1;  
-                        mon=mon+12;  
-                    }  
-                    if(mon<10){  
-                        mon="0"+mon;  
-                    } 
-                last6Month[0]=year+"/"+mon; 
-                for(var i=1;i<6;i++){  
-                    mon=mon-1;  
-                    if(mon<=0){  
-                        year=year-1;  
-                        mon=mon+12;  
-                    }  
-                    if(mon<10){  
-                        mon="0"+mon;  
-                    }  
-                      
-                    last6Month[i]=year+"/"+mon;  
-                }  
-                  
-                return last6Month.reverse();
-	
+	var data = new Date();
+	//获取年  
+	var year = data.getFullYear();
+	//获取月  
+	var mon = data.getMonth() + 1;
+	if(mon <= 0) {
+		year = year - 1;
+		mon = mon + 12;
+	}
+	if(mon < 10) {
+		mon = "0" + mon;
+	}
+	last6Month[0] = year + "/" + mon;
+	for(var i = 1; i < 6; i++) {
+		mon = mon - 1;
+		if(mon <= 0) {
+			year = year - 1;
+			mon = mon + 12;
+		}
+		if(mon < 10) {
+			mon = "0" + mon;
+		}
+
+		last6Month[i] = year + "/" + mon;
+	}
+
+	return last6Month.reverse();
+
 	return last6Month;
-	
+
 }
 
-function get10MinutesScale()
-{
+function get10MinutesScale() {
 	var currDate = new Date();
-	var odd = currDate.getMinutes()%10;
+	var odd = currDate.getMinutes() % 10;
 	var returnArr = new Array();
-	currDate.setMinutes(currDate.getMinutes()-odd);
-	for(var i = 0; i <7; i++){
-		returnArr.push(currDate.getHours()+":"+(currDate.getMinutes()<10?("0"+currDate.getMinutes()):currDate.getMinutes()));
-		currDate.setMinutes(currDate.getMinutes()-10);
+	currDate.setMinutes(currDate.getMinutes() - odd);
+	for(var i = 0; i < 7; i++) {
+		returnArr.push(currDate.getHours() + ":" + (currDate.getMinutes() < 10 ? ("0" + currDate.getMinutes()) : currDate.getMinutes()));
+		currDate.setMinutes(currDate.getMinutes() - 10);
 	}
 	return returnArr;
 }
 
-
-function getLatestDays(num)
-{
+function getLatestDays(num) {
 	var currentDay = new Date();
 	var returnDays = [];
 	currentDay.setDate(currentDay.getDate());
-	returnDays.push((currentDay.getMonth()+1)+"/"+currentDay.getDate());
-	for (var i = 0 ; i < num - 1 ; i++)
-	{
+	returnDays.push((currentDay.getMonth() + 1) + "/" + currentDay.getDate());
+	for(var i = 0; i < num - 1; i++) {
 		currentDay.setDate(currentDay.getDate() - 1);
-		returnDays.push((currentDay.getMonth()+1)+"/"+currentDay.getDate());
+		returnDays.push((currentDay.getMonth() + 1) + "/" + currentDay.getDate());
 	}
 	return returnDays.reverse();
 }
 
-function drawLayer02Label(canvasObj,text,textBeginX,lineEndX){
+function drawLayer02Label(canvasObj, text, textBeginX, lineEndX) {
 	var colorValue = '#04918B';
 
 	var ctx = canvasObj.getContext("2d");
 
 	ctx.beginPath();
-	ctx.arc(35,55,2,0,2*Math.PI);
+	ctx.arc(35, 55, 2, 0, 2 * Math.PI);
 	ctx.closePath();
 	ctx.fillStyle = colorValue;
 	ctx.fill();
 
-	ctx.moveTo(35,55);
-	ctx.lineTo(60,80);
-	ctx.lineTo(lineEndX,80);
+	ctx.moveTo(35, 55);
+	ctx.lineTo(60, 80);
+	ctx.lineTo(lineEndX, 80);
 	ctx.lineWidth = 1;
 	ctx.strokeStyle = colorValue;
 	ctx.stroke();
 
-	ctx.font='12px Georgia';
+	ctx.font = '12px Georgia';
 	ctx.fillStyle = colorValue;
-	ctx.fillText(text,textBeginX,92);
+	ctx.fillText(text, textBeginX, 92);
 }
